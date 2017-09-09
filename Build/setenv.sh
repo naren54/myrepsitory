927600eb8c672121980e2a7d0f99cf85cf8c410f
root <chennunarenfmw@gmail.com>
2017-09-09 17:53:26 +0530

    This is my first commit

---
+++ Build/setenv.sh
@@ -0,0 +1,8 @@
+#! /bin/sh
+export ORACLE_HOME=/u01/app/Fusion/Middleware
+export ANT_HOME=/u01/app/Fusion/Middleware/modules/org.apache.ant_1.7.1
+export PATH=$ANT_HOME/bin:$PATH
+export JAVA_HOME=/u01/app/Fusion/Middleware/jrockit-jdk1.6.0_33-R28
+export CURRENT_FOLDER=`pwd`
+export PATH=$PATH:$JAVA_HOME/bin
+
